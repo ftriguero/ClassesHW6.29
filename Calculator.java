b0de6c580ad606ec75865c13f5b8fc3e37606818
ftriguero <84296453+ftriguero@users.noreply.github.com>
2021-07-04 23:40:38 -0700

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+
+public class Calculator {
+	
+	
+	
+	public static int add(int num1,int num2) {
+		return num1 + num2;
+	}
+	
+	public static int subtraction(int num1,int num2) {
+		return num1 - num2;
+	}
+	
+	public static int multiplication(int num1,int num2) {
+		return num1 * num2;
+	}
+	
+	public static int division(int num1,int num2) {
+		return num1 / num2;
+	}
+	
+	public static int square(int num1,int num2) {
+		return num1 * num2;
+	}
+}
